743ca24b12c526556ef0cff924e5be61a58352d6
Silverteal <67404722+Silverteal@users.noreply.github.com>
2023-12-09 23:07:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,14 +39,25 @@ failed = 0
 with open("arguments.txt") as f:
    api_keys = f.read().strip().split(',')
 
-for api_key in api_keys:
+assert api_keys
+
+for i,api_key in enumerate(api_keys,start=1):
     # api_key = api_keys[0]
+    print('==============================')
+    print(f'任务第{i}项，共{len(api_keys)}项')
+    print(f'APIKEY的长度是{len(api_key)}')
     info_before = get_user_info(api_key)
+
+    if isinstance(info_before['data'],str):
+        print('无法获取用户信息')
+        print('==============================')
+        failed += 1
+        continue
+    
     points_before = info_before['data']['Points']
     ID = info_before['data']['ID']
     name = info_before['data']['Name']
 
-    print('==============================')
     print(f'ID：{ID}')
     print(f'用户名：{name[0]}***{name[-1]}')
     print(f'签到前积分：{points_before}')
